c8456921a0f6897fd4d4cb4cce6f5fd1e8d6c9cd
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-05-05 23:30:58 +0900

    css loader에 대한 설명

--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@
  * 
  * 
  */
+
+/**
+ * 
+ * * 설명
+ * * css '작성 방식'을 번들링에서 사용하려면
+ * * css-loader, style-loader가 필요하다.
+ * * 해당 entry point 파일에 css 파일을 import 해야한다.
+ * * 굳이 변수가 필요없으므로, require() commonJS 방식으로 읽어 들였다.
+ */
 require("./assets/css/index.css");
 require("./assets/css/bundle-test-1.css");
