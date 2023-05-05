7dcfb536343c81848e4f47175b609f56aeb53d32
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-05-05 23:22:32 +0900

    주석 제거

--- webpack.config.js
+++ webpack.config.js
@@ -14,79 +14,37 @@
  * @property {function} resolve - 상대 경로를 절대 경로로 변환하는 함수.
  */
 
-const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
-// work : copy-webpack-plugin은 서버사이드 멀티페이지 방식으로 송출할 때 사용
-// work : clinet-side 방식이라면 copy-webpack-plugin은 필요없다.
 const path = require("path");
 
 module.exports = {
 
-  // * @property {string} mode - 빌드 모드. 'development' 또는 'production' 중 선택.
+  // * 배포, 개발 모드 설정
   mode: "production",
-  // * @property {string} entry - 애플리케이션의 진입점. JS 파일 경로.
+  // * load(적재) 할 메인 파일(진입점)
   entry: "./src/index.js",
-
-  // * @property {Object} output - 번들링 결과물 설정 객체.
   output: {
-    // * @property {string} output.path - 번들링 결과물이 생성될 경로. 절대경로로 지정.
-    // ! resolve() : node.js commonJS 메서드 상대경로를 절대경로로 변환
     path: path.resolve(__dirname, "dist"), 
-    // * @property {string} output.filename - 생성된 번들링 파일의 이름.
+    // * 번들링 완료된 파일이름
     filename: "./index.bundle.js"
-
   },
-
-
 
   plugins: [
 
-
-
-
-
-    // * @property {Object} plugins - 웹팩 플러그인 설정 객체.
     new HtmlWebpackPlugin({
-      // * @property {string} template - 템플릿 HTML 파일 경로.
+      // * load(적재) 할 html 파일의 경로
       template: "./src/assets/index.html",
-      // * @property {string} filename - 결과 파일 이름.
-      filename: "test.html"
+      // * 번들링 완료된 파일이름
+      filename: "index.bundle.html"
     }),
-
-    
-  
-
-
-    // // * @property {Object} plugins - 웹팩 플러그인 설정 객체.
-    // new CopyWebpackPlugin({
-    //   // * @property {Array} patterns - 복사할 파일 목록.
-    //   patterns: [
-    //     {
-    //       // * @property {string} from - 복사할 파일 경로.
-    //       from: "./src/assets",
-    //       // * @property {string} to - 복사될 파일 경로.
-    //       to: "./assets"
-    //     }
-    //   ]
-    // }),
-
-
-
-
-
   ],
 
   module : {
     rules: [
       {
-        // * @property {RegExp} test - 로더가 적용될 파일 경로를 지정하는 정규식.
         test: /\.css$/,
-        // * @property {Array} use - 적용할 로더를 설정.
-        // ! 로더는 오른쪽에서 왼쪽 순서로 적용됨.
-        // ! css-loader : css 파일을 읽어서 자바스크립트에서 사용할 수 있게 변환
-        // ! style-loader : 자바스크립트로 변경된 스타일을 동적으로 돔에 추가
-        // * css는 '마크업' 이고, style객체는 DOM API key이기 때문에 적재하는 순서가 중요하다.
+        // * css 파일을 읽어들이기 위해 필요한 로더
+        // * 배열의 맨 마지막 로더가 가장 먼저 실행됨
         use: ["style-loader", "css-loader"]
       }
     ]
