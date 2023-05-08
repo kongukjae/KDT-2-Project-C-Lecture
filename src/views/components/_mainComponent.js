e94fa6328d6aa7e853e13852ede6eae6011f352a
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-05-08 23:42:05 +0900

    메인이 될 컴포넌트, 리액트의 component 개념

---
+++ src/views/components/_mainComponent.js
@@ -0,0 +1,44 @@
+/**
+ * 메인 컴포넌트
+ * @param {string} tagName - 태그 이름
+ * @param {object} attributes - 속성 객체
+ * @param {array} children - 자식 요소
+ * @returns {string} HTML 문자열
+ * @example
+ * mainComponent({
+ *  tagName: "div",
+ *  attributes: {
+ *    id: "main",
+ *    className: "container",
+ *  },
+ *  children: [
+ *    mainComponent({
+ *      tagName: "h1",
+ *      attributes: {
+ *      className: "title",
+ *    })
+ *  ]
+ *
+ */
+
+module.exports = ({ tagName = "div", attributes = {}, children = [] } = {}) => {
+  // 태그 속성을 문자열로 변환하는 변수
+  let attributeString = "";
+
+  for (const [key, value] of Object.entries(attributes)) {
+    // 반복자를 구조분해할당으로 키와 값으로 나누어서 사용할 수 있다.
+    attributeString += ` ${key}="${value}"`;
+  }
+  // 태그의 자식요소를 문자열로 변환하는 변수
+  let childrenString = "";
+
+  for (const child of children) {
+    childrenString += child(); // 반복자방식으로 children배열 원소값을 함수로 실행시킬 수 있다.
+  }
+
+  return `
+    <${tagName}${attributeString}>
+      ${childrenString}
+    </${tagName}>
+  `;
+};
