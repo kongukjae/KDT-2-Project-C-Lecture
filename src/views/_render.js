8dd8b62418629196f73ea44daf4ff4d2eb2b0487
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-05-08 23:08:54 +0900

    view 부분 render.js 코어 컴포넌트 제작

---
+++ src/views/_render.js
@@ -0,0 +1,14 @@
+module.exports = function(title, components) {
+  const content = components.map(component => component()).join('');
+
+  return `
+    <html>
+      <head>
+        <title>${title}</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `;
+}
