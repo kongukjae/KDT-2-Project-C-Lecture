690917a42cba750054c547e4d2bb697acfb988cc
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-05-09 23:38:41 +0900

    GET 처리 관련 한 아이디어 간단하게 주석처리, 나중에 프로젝트로 옮겨 놓기

--- src/app.js
+++ src/app.js
@@ -8,7 +8,14 @@
 
 module.exports = function (request, response) {
   if(request.method === "GET") {
+    // 메인페이지에 해당하는 컴포넌트를 조립한다.
+    // 노션, VScode 처럼 좌측에 메뉴바 형식으로 메인 매뉴를 처리하고,
+    // 메인 메뉴에 선택된 중간메뉴(2depth)에 해당하는 컴포넌트를 조립한다.
+    // 최종적으로는 우측에 컨텐츠 영역에 해당하는 컴포넌트를 조립한다.
 
+    // 해당 부분에 조립 및 response 처리
+
+    // 어지간한건 모두 번들링해서 보내주는것이 좋겠다.
   }
   else if(request.method === "POST") {
   }
